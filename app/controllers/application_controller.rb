b8928bd7435a5d1aabb45abf2024a0ebd59bb689
TakuKunimatsu <97439488+TakuKunimatsu@users.noreply.github.com>
2022-05-14 18:31:53 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+  
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"] 
+    end
+  end
+  
   protected
 
   def devise_parameter_sanitizer
@@ -7,7 +17,7 @@ class ApplicationController < ActionController::Base
     elsif resource_class ==AdminUser
       AdminUser::ParameterSanitizer.new(AdminUser, :admin_user, params)
     else
-      super # Use the default one
+      super
     end
   end
 end
