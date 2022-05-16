f1849420efe6efb139ce55884c4e155a84853b73
TakuKunimatsu <97439488+TakuKunimatsu@users.noreply.github.com>
2022-05-16 21:25:45 +0900

    バリデーションの追加

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -7,6 +7,25 @@ class Performance < ApplicationRecord
   validates :info,            presence: true
   validates :price,           presence: true, numericality: { only_integer: true }
   validates :image,           presence: true
-  
+  validate :date_before_start
+  validate :date_before_last
+  validate :start_last_check
+
+  def date_before_start
+    return if start_day.blank?
+    errors.add(:start_day, "は今日以降のものを選択してください") if start_day < Date.today
+  end
+
+  def date_before_last
+    return if last_day.blank? || start_day.blank?
+    errors.add(:last_day, "は開始日以降のものを選択してください") if last_day < start_day
+  end
+
+  def start_end_check
+    errors.add(:last_day, "の日付を正しく記入してください。") unless
+    self.start_day < self.end_day
+  end
+
+
   belongs_to :admin_user
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,7 +2,7 @@ class Schedule < ApplicationRecord
 
   validates :performance_day,       presence: true
   validates :performance_time,      presence: true
-  validates :stock,                 presence: true
+  validates :stock,                 presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000 }
 
   belongs_to :performance
 end
