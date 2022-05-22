844f8f76a4da2fca1fb41bb26b0696e6a115b919
TakuKunimatsu <97439488+TakuKunimatsu@users.noreply.github.com>
2022-05-22 20:05:14 +0900

    重複したバリデーションを削除

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -6,10 +6,13 @@ class Performance < ApplicationRecord
   validates :last_day,        presence: true
   validates :info,            presence: true
   validates :price,           presence: true, numericality: { only_integer: true }
-  validates :image,           presence: true
+  validates :image,          presence: true
   validate :date_before_start
   validate :date_before_last
-  validate :start_last_check
+
+  has_one_attached :image
+  has_many :schedules
+  belongs_to :admin_user
 
   def date_before_start
     return if start_day.blank?
@@ -21,11 +24,4 @@ class Performance < ApplicationRecord
     errors.add(:last_day, "は開始日以降のものを選択してください") if last_day < start_day
   end
 
-  def start_end_check
-    errors.add(:last_day, "の日付を正しく記入してください。") unless
-    self.start_day < self.end_day
-  end
-
-  has_many :schedules
-  belongs_to :admin_user
 end
