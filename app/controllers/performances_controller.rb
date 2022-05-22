cd27df3e507db0ecc1cba84075b0eb8901b9e71a
TakuKunimatsu <97439488+TakuKunimatsu@users.noreply.github.com>
2022-05-22 20:04:54 +0900

    createアクションを作成

--- app/controllers/performances_controller.rb
+++ app/controllers/performances_controller.rb
@@ -1,4 +1,28 @@
 class PerformancesController < ApplicationController
-  def index
+  
+  #def index
+  #end
+
+  def new
+    @performance= Performance.new
   end
+
+  def create
+    @performance = Performance.new(performance_params)
+    if @performance.valid? 
+      @performance.save
+      redirect_to root_path    
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def performance_params
+    params.require(:performance).permit(:name, :theater, :start_day, :last_day, :info, :price, :image).merge(admin_user_id: current_admin_user.id)
+  end
+
+  
 end
